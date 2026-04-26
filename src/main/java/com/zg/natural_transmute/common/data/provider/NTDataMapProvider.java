882d5773fe4eb5637bb4799eac8f9ac7025959cd
秋雨落 <i@rain.cx>
2026-04-26 13:27:22 +0800

    Remove datamap for fuel energy.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/com/zg/natural_transmute/common/data/provider/NTDataMapProvider.java
+++ src/main/java/com/zg/natural_transmute/common/data/provider/NTDataMapProvider.java
@@ -1,8 +1,5 @@
 package com.zg.natural_transmute.common.data.provider;
 
-import com.zg.natural_transmute.common.blocks.entity.HarmoniousChangeStoveBlockEntity;
-import com.zg.natural_transmute.common.datamaps.HarmoniousChangeFuel;
-import com.zg.natural_transmute.registry.NTDataMaps;
 import com.zg.natural_transmute.registry.NTItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
@@ -24,10 +21,6 @@ public class NTDataMapProvider extends DataMapProvider {
     @Override
     protected void gather(HolderLookup.Provider provider) {
         this.addCompostable(0.65F, NTItems.WARPED_WART.get());
-        Builder<HarmoniousChangeFuel, Item> hcFuels = this.builder(NTDataMaps.HARMONIOUS_CHANGE_FUELS);
-        HarmoniousChangeStoveBlockEntity.buildFuels((value, amount) -> value
-                .ifLeft(item -> hcFuels.add(item.builtInRegistryHolder(), new HarmoniousChangeFuel(amount), false))
-                .ifRight(tag -> hcFuels.add(tag, new HarmoniousChangeFuel(amount), false)));
     }
 
     public void addCompostable(float chance, ItemLike item) {
--- src/main/java/com/zg/natural_transmute/registry/NTDataMaps.java
+++ src/main/java/com/zg/natural_transmute/registry/NTDataMaps.java
@@ -1,15 +1,4 @@
 package com.zg.natural_transmute.registry;
 
-import com.zg.natural_transmute.NaturalTransmute;
-import com.zg.natural_transmute.common.datamaps.HarmoniousChangeFuel;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.Item;
-import net.neoforged.neoforge.registries.datamaps.DataMapType;
-
 public class NTDataMaps {
-
-    public static final DataMapType<Item, HarmoniousChangeFuel> HARMONIOUS_CHANGE_FUELS = DataMapType.builder(
-            NaturalTransmute.prefix("harmonious_change"), Registries.ITEM, HarmoniousChangeFuel.CODEC)
-            .synced(HarmoniousChangeFuel.AMOUNT_CODEC, false).build();
-
 }
